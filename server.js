bd9d00cd9def6a844392148f8aeeedb24e587439
John Joo <john@Johns-iMac.local>
2018-05-15 17:25:58 -0700

    added app.put recipe

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.put('/shopping-list/:id', jsonParser, (req, res) => {
     }
   }
 
-  app.put('/recipes/:id', jsonParser, (req, res) => {
+app.put('/recipes/:id', jsonParser, (req, res) => {
     const requiredFields = ['name', 'ingredients'];
     for (let i=0; i<requiredFields.length; i++) {
       const field = requiredFields[i];
